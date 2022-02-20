499c1313feff7f010d1eb831c42dd5dfd29da3f8
Dhileepan C R <dhileepan@Dhileepans-MacBook-Pro.local>
2022-02-20 16:36:20 -0600

    created retirement info data

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -1,1 +1,23 @@
 SELECT * FROM departments;
+
+-- Retirement eligibility
+SELECT Count(first_name), last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Retirement count
+SELECT Count(emp_no)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Inserting Data into retirement_info Table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Select from retirement Info
+SELECT * FROM retirement_info;
