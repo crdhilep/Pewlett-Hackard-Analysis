a598392575da5a91ec03b1d377ecf8813e8c4323
Dhileepan C R <dhileepan@Dhileepans-MacBook-Pro.local>
2022-02-20 17:10:25 -0600

    uploading challenge

---
+++ Queries/Employee_Database_Challenge.sql
@@ -0,0 +1,54 @@
+-- Deliverable 1 --
+
+-- Extract Data from Employee & Titles table and store it in retirement_titles
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+    INNER JOIN titles as t
+	ON (e.emp_no = t.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Extract Data retirement_titles and store it in unique_titles
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+-- Extract Data unique_titles and store it in retiring_titles
+SELECT COUNT(ut.emp_no), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY COUNT (ut.emp_no) DESC;
+
+-- End Deliverable 1 --
+
+
+-- Deliverable 2 --
+
+-- Extract Data employees, dept_emp, titles and store it in mentorship_eligibility
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibility
+FROM employees as e
+    INNER JOIN dept_emp as de
+    ON (e.emp_no = de.emp_no)
+    INNER JOIN titles as t
+    ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
